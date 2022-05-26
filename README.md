53f62939810026a60b7c3cfd7f2a548f6d3fcb9d
Sanket Singh <sanketsingh@Sankets-MacBook-Pro.local>
2022-05-27 01:35:24 +0530

    Minor Dep: Configured dotenv

--- README.md
+++ README.md
@@ -9,6 +9,8 @@ E-commerce APi project in NodeJS
     - Clone the repository
     - Go inside the cloned folder from your terminal/cmd
     - Run `npm install`
+    - Create a new file `.env` and then configure the following environment variables
+        - PORT=3000
 
 - Run the Project
     - Run `node server.js`
--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express'); // Package to get the express object constructor
 const bodyParser = require('body-parser');
+require('dotenv').config();
 
 const app = express(); // this function returns an express object which has the capabilitites to handle server side requests
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-const PORT = 3000; // this will be the port on our local system where server will run
+const PORT = process.env.PORT; // this will be the port on our local system where server will run
 
 
 app.listen(PORT, () => { // listen function registers the app for the port
