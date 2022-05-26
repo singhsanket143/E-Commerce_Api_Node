e0a8e4f95bbf0d0876f8e00e6553ac4ac245ec1a
Sanket Singh <sanketsingh@Sankets-MacBook-Pro.local>
2022-05-27 01:20:54 +0530

    Feature: Added express setup with bodyparser and added README

---
+++ README.md
@@ -0,0 +1,14 @@
+E-commerce APi project in NodeJS
+
+- System Requirements 
+    - NodeJS
+    - MySQL
+    - NPM
+
+- Setting up the project
+    - Clone the repository
+    - Go inside the cloned folder from your terminal/cmd
+    - Run `npm install`
+
+- Run the Project
+    
---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express'); // Package to get the express object constructor
+const bodyParser = require('body-parser');
+
+const app = express(); // this function returns an express object which has the capabilitites to handle server side requests
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+const PORT = 3000; // this will be the port on our local system where server will run
+
+
+app.listen(PORT, () => { // listen function registers the app for the port
+    console.log(`Server started on Port ${PORT}`)
+})
