6328da00331691c2ff849587ea91a162148d19db
Sanket Singh <sanketsingh@Sankets-MacBook-Pro.local>
2022-05-28 00:56:40 +0530

    Fix: Updated the routes

--- routes/category.routes.js
+++ routes/category.routes.js
@@ -3,36 +3,36 @@ const categoryValidator = require('../middlewares/category.validator');
 
 const routes = (app) => {
     app.post(
-        '/ecom/api/v1/category', 
+        '/ecom/api/v1/categories', 
         categoryValidator.validateCreate,
         categoryController.createCategory
     );
 
     app.get(
-        '/ecom/api/v1/category', 
+        '/ecom/api/v1/categories', 
         categoryController.getAllCategories
     );
 
     app.get(
-        '/ecom/api/v1/category/:id',
+        '/ecom/api/v1/categories/:id',
         categoryValidator.validateGetById,
         categoryController.getCategoryById
     )
 
     app.put(
-        '/ecom/api/v1/category/:id',
+        '/ecom/api/v1/categories/:id',
         categoryValidator.validateUpdate,
         categoryController.updateCategory
     )
 
     app.patch(
-        '/ecom/api/v1/category/:id',
+        '/ecom/api/v1/categories/:id',
         categoryValidator.validatePartialUpdate,
         categoryController.updateCategory
     )
 
     app.delete(
-        '/ecom/api/v1/category/:id',
+        '/ecom/api/v1/categories/:id',
         categoryController.deleteCategory
     )
 }
